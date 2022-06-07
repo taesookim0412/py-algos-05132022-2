3ce37263a32f1072e8c35a692fa7978206418100
taesookim0412 <taesoo.kim0412@gmail.com>
2022-06-06 22:40:52 -0700

    8 - reviews - fixes

--- algos/23_bfs_818_race_car.py
+++ algos/23_bfs_818_race_car.py
@@ -1,6 +1,7 @@
 # lesson learned: greedily using DFS can lead to very incorrect solutions:
 # use bfs instead when possible.
 
+# fixed up and incorrect, greedy solution
 # class Solution:
 #     def racecar(self, target: int) -> int:
 #         path = []
@@ -9,21 +10,23 @@
 #         self.speed = 1
 #         self.target = target
 #         self.dfs(path, dp)
-#         # print(dp)
+#         print(dp)
 #         return dp[target]
 #
 #     # dp[pos] = min(dp[pos], len(path))
 #     def dfs(self, path, dp):
-#         if self.pos == -1 or len(path) > self.target:
+#         if self.pos == -1 or len(path) > self.target * 3:
 #             return 999999
 #         if self.pos in dp:
 #             return dp[self.pos]
+#
+#         minLen = 99999
 #         # instruction: 'A'
 #         tempPos = self.pos
 #         tempSpeed = self.speed
 #         self.pos += self.speed
 #         self.speed *= 2
-#         self.dfs(path + ['A'], dp)
+#         minLen = min(minLen, self.dfs(path + ['A'], dp) - 1)
 #
 #         self.pos = tempPos
 #         self.speed = tempSpeed
@@ -33,10 +36,13 @@
 #             self.speed = -1
 #         else:
 #             self.speed = 1
-#         self.dfs(path + ['R'], dp)
+#         minLen = min(minLen, self.dfs(path + ['R'], dp) - 1)
 #         self.speed = tempSpeed
 #
 #         if self.pos in dp:
-#             dp[self.pos] = min(self.pos, len(path))
+#             dp[self.pos] = min(dp[self.pos], minLen)
 #         else:
-#             dp[self.pos] = len(path)
+#             dp[self.pos] = minLen
+#         print(f"returning {minLen} from {self.pos}, {path}")
+#
+#         return len(path)
